e2f994b7e311d8516e51535aa14c434f9392a9b0
Noah Heldman <noah.heldman@gmail.com>
2011-02-07 12:08:15 -0800

    Base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharDb::Application.config.secret_token = '30448ffa05d1b6b33d9eeb92b96bab5fb1e0c964dc6fd70f8e3e40ecb8ee97ac62f38fec35144c1c02906818dfb4848f977cdbb99acef0f9d78a5b0ff5386f9f'
